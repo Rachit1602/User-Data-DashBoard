89ad93d2cba6ac0c5388de60bd2cb1ba1f6fce9a
Rachit Narang <rachit@Rachits-MacBook-Air.local>
2025-01-20 01:48:02 +0530

    some changes

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,9 @@ const __dirname = path.dirname(__filename);
 
 const allowedorigin=[
     "http://localhost:3000",
-    "https://user-data-dash-board-rachit1602s-projects.vercel.app/",
-    "https://user-data-dash-board-git-main-rachit1602s-projects.vercel.app/",
-    "https://user-data-dash-board.vercel.app/"
+    "https://user-data-dash-board-rachit1602s-projects.vercel.app",
+    "https://user-data-dash-board-git-main-rachit1602s-projects.vercel.app",
+    "https://user-data-dash-board.vercel.app"
 ]
 connect();
 app.use(cors({
